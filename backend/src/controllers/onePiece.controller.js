2a61b5b76a90c00cd9fa774d0a1fcb9fe71768cc
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-02-04 21:26:11 -0300

    devolve imagem aleatório no controller

--- backend/src/controllers/onePiece.controller.js
+++ backend/src/controllers/onePiece.controller.js
@@ -1,8 +1,9 @@
 
+import onePieceService from "../services/onePiece.service.js"
 
 const getRandomImage = (req, res)=> {
   res.send({
-    message: 'https://images.dog.ceo/breeds/malinois/n02105162_4426.jpg',
+    message: onePieceService.getRandomImage(),
     status: 'success'
   })
 }
