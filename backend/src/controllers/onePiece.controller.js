55b6907d4437a09f9fe700e18e15399fcaa74ded
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-02-04 21:51:52 -0300

    add pasta frontend

--- backend/src/controllers/onePiece.controller.js
+++ backend/src/controllers/onePiece.controller.js
@@ -1,13 +1,13 @@
 
 import onePieceService from "../services/onePiece.service.js"
+import { STATUS } from '../utils/statusCode.js'
 
 const getRandomImage = (req, res)=> {
-  res.status(200).send({
+  res.status(STATUS.OK.CODE).send({
     message: onePieceService.getRandomImage(),
-    status: 'success'
+    status: STATUS.OK.MSG
   })
 }
-
 
 export default {
   getRandomImage
